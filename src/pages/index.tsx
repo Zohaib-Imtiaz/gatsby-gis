b5c2cf6a8589191e662d6e78ea0bc9ff1c15bd44
MUHAMMAD ZOHAIB IMTIAZ <zohaib_phone@outlook.com>
2023-09-07 04:22:36 +0500

    main page now map page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,193 +1,165 @@
-import * as React from "react"
-import type { HeadFC, PageProps } from "gatsby"
+import * as React from "react";
+import MapboxReact from "../Mapbox/instance";
+import { createClient } from "@supabase/supabase-js";
+import {
+  countriesPolygonClustersLayer,
+  selectedCountryPolygonClustersLayer,
+} from "../Mapbox/layers/mapPageLayers";
+import CountryPopUp from "../components/MapPopup/CountryPopUp";
+import Popup from "../Mapbox/mapComponents/Popup";
+import { LngLatLike } from "mapbox-gl";
+import { ShapeLoader, WordLoader } from "../components/Loader/Loader";
 
-const pageStyles = {
-  color: "#232129",
-  padding: 96,
-  fontFamily: "-apple-system, Roboto, sans-serif, serif",
-}
-const headingStyles = {
-  marginTop: 0,
-  marginBottom: 64,
-  maxWidth: 320,
-}
-const headingAccentStyles = {
-  color: "#663399",
-}
-const paragraphStyles = {
-  marginBottom: 48,
-}
-const codeStyles = {
-  color: "#8A6534",
-  padding: 4,
-  backgroundColor: "#FFF4DB",
-  fontSize: "1.25rem",
-  borderRadius: 4,
-}
-const listStyles = {
-  marginBottom: 96,
-  paddingLeft: 0,
-}
-const doclistStyles = {
-  paddingLeft: 0,
-}
-const listItemStyles = {
-  fontWeight: 300,
-  fontSize: 24,
-  maxWidth: 560,
-  marginBottom: 30,
-}
+const supabase = createClient(
+  process.env.GATSBY_SUPABASE_URL as string,
+  process.env.GATSBY_SUPABASE_KEY as string
+);
 
-const linkStyle = {
-  color: "#8954A8",
-  fontWeight: "bold",
-  fontSize: 16,
-  verticalAlign: "5%",
-}
-
-const docLinkStyle = {
-  ...linkStyle,
-  listStyleType: "none",
-  display: `inline-block`,
-  marginBottom: 24,
-  marginRight: 12,
-}
-
-const descriptionStyle = {
-  color: "#232129",
-  fontSize: 14,
-  marginTop: 10,
-  marginBottom: 0,
-  lineHeight: 1.25,
-}
-
-const docLinks = [
-  {
-    text: "TypeScript Documentation",
-    url: "https://www.gatsbyjs.com/docs/how-to/custom-configuration/typescript/",
-    color: "#8954A8",
-  },
-  {
-    text: "GraphQL Typegen Documentation",
-    url: "https://www.gatsbyjs.com/docs/how-to/local-development/graphql-typegen/",
-    color: "#8954A8",
+const MapPage = () => {
+  if (typeof window === "undefined") {
+    // Code that uses `document` or browser-specific APIs
+    console.log('OK')
+    return <></>
   }
-]
+  const [countriesBoundries, setCountiesBoundries] = React.useState<
+    FeatureCollection | undefined
+  >(undefined);
+  const [selectedCountryISO3, setSelectedCountryISO3] = React.useState<
+    string | undefined
+  >(undefined);
+  const [lngLat, setLngLat] = React.useState<LngLatLike | null>(null);
+  const [dataLoading, setDataLoading] = React.useState(true);
+  const [overviewData, setOverviewData] = React.useState<
+    { Value: any; Indicator: { title: any; units: any }[] }[] | null
+  >(null);
+  const [overviewDataFetchLoading, setOverviewDataFetchLoading] =
+    React.useState(true);
 
-const badgeStyle = {
-  color: "#fff",
-  backgroundColor: "#088413",
-  border: "1px solid #088413",
-  fontSize: 11,
-  fontWeight: "bold",
-  letterSpacing: 1,
-  borderRadius: 4,
-  padding: "4px 6px",
-  display: "inline-block",
-  position: "relative" as "relative",
-  top: -2,
-  marginLeft: 10,
-  lineHeight: 1,
-}
+  console.log("BOUNDRIES===>", countriesBoundries);
 
-const links = [
-  {
-    text: "Tutorial",
-    url: "https://www.gatsbyjs.com/docs/tutorial/getting-started/",
-    description:
-      "A great place to get started if you're new to web development. Designed to guide you through setting up your first Gatsby site.",
-    color: "#E95800",
-  },
-  {
-    text: "How to Guides",
-    url: "https://www.gatsbyjs.com/docs/how-to/",
-    description:
-      "Practical step-by-step guides to help you achieve a specific goal. Most useful when you're trying to get something done.",
-    color: "#1099A8",
-  },
-  {
-    text: "Reference Guides",
-    url: "https://www.gatsbyjs.com/docs/reference/",
-    description:
-      "Nitty-gritty technical descriptions of how Gatsby works. Most useful when you need detailed information about Gatsby's APIs.",
-    color: "#BC027F",
-  },
-  {
-    text: "Conceptual Guides",
-    url: "https://www.gatsbyjs.com/docs/conceptual/",
-    description:
-      "Big-picture explanations of higher-level Gatsby concepts. Most useful for building understanding of a particular topic.",
-    color: "#0D96F2",
-  },
-  {
-    text: "Plugin Library",
-    url: "https://www.gatsbyjs.com/plugins",
-    description:
-      "Add functionality and customize your Gatsby site or app with thousands of plugins built by our amazing developer community.",
-    color: "#8EB814",
-  },
-  {
-    text: "Build and Host",
-    url: "https://www.gatsbyjs.com/cloud",
-    badge: true,
-    description:
-      "Now you’re ready to show the world! Give your Gatsby site superpowers: Build and host on Gatsby Cloud. Get started for free!",
-    color: "#663399",
-  },
-]
+  const fetchFromDB = React.useCallback(async () => {
+    try {
+      let { data, error } = await supabase.rpc(
+        "get_boundries_with_name_currency",
+        {
+          take: 250,
+        }
+      );
+      if (error) throw error;
 
-const IndexPage: React.FC<PageProps> = () => {
-  return (
-    <main style={pageStyles}>
-      <h1 style={headingStyles}>
-        Congratulations
-        <br />
-        <span style={headingAccentStyles}>— you just made a Gatsby site! 🎉🎉🎉</span>
-      </h1>
-      <p style={paragraphStyles}>
-        Edit <code style={codeStyles}>src/pages/index.tsx</code> to see this page
-        update in real-time. 😎
-      </p>
-      <ul style={doclistStyles}>
-        {docLinks.map(doc => (
-          <li key={doc.url} style={docLinkStyle}>
-            <a
-              style={linkStyle}
-              href={`${doc.url}?utm_source=starter&utm_medium=ts-docs&utm_campaign=minimal-starter-ts`}
-            >
-              {doc.text}
-            </a>
-          </li>
-        ))}
-      </ul>
-      <ul style={listStyles}>
-        {links.map(link => (
-          <li key={link.url} style={{ ...listItemStyles, color: link.color }}>
-            <span>
-              <a
-                style={linkStyle}
-                href={`${link.url}?utm_source=starter&utm_medium=start-page&utm_campaign=minimal-starter-ts`}
-              >
-                {link.text}
-              </a>
-              {link.badge && (
-                <span style={badgeStyle} aria-label="New Badge">
-                  NEW!
-                </span>
-              )}
-              <p style={descriptionStyle}>{link.description}</p>
-            </span>
-          </li>
-        ))}
-      </ul>
-      <img
-        alt="Gatsby G Logo"
-        src="data:image/svg+xml,%3Csvg width='24' height='24' fill='none' xmlns='http://www.w3.org/2000/svg'%3E%3Cpath d='M12 2a10 10 0 110 20 10 10 0 010-20zm0 2c-3.73 0-6.86 2.55-7.75 6L14 19.75c3.45-.89 6-4.02 6-7.75h-5.25v1.5h3.45a6.37 6.37 0 01-3.89 4.44L6.06 9.69C7 7.31 9.3 5.63 12 5.63c2.13 0 4 1.04 5.18 2.65l1.23-1.06A7.959 7.959 0 0012 4zm-8 8a8 8 0 008 8c.04 0 .09 0-8-8z' fill='%23639'/%3E%3C/svg%3E"
+      const features: Feature[] = [];
+      for (const element of data) {
+        features.push({
+          type: "Feature",
+          geometry: JSON.parse(element.geojson),
+          properties: {
+            iso3: element.iso3,
+            name: element.name,
+            currency_code: element.currency_code,
+          },
+        });
+      }
+      setCountiesBoundries({
+        type: "FeatureCollection",
+        features: features,
+      });
+      setDataLoading(false);
+    } catch (error) {
+      console.error("Catched Error ===>", error);
+    }
+  }, []);
+
+  React.useEffect(() => {
+    fetchFromDB();
+  }, []);
+
+  const updateSelectedCountry = React.useCallback(
+    (data: mapboxgl.MapboxGeoJSONFeature[], position: LngLatLike) => {
+      setOverviewDataFetchLoading(true);
+      setSelectedCountryISO3(data[0].properties?.iso3);
+      setLngLat(position);
+    },
+    []
+  );
+
+  const selectedCountry = React.useMemo(() => {
+    const selectedFeature = countriesBoundries?.features.find(
+      (feat) => feat.properties.iso3 === selectedCountryISO3
+    );
+    console.log(selectedCountryISO3, selectedFeature, countriesBoundries);
+    if (selectedFeature) {
+      return {
+        type: "FeatureCollection",
+        features: [selectedFeature],
+      } as FeatureCollection;
+    }
+    return undefined;
+  }, [selectedCountryISO3]);
+
+  const popUp = React.useCallback(() => {
+    if (overviewDataFetchLoading) {
+      return <WordLoader />;
+    }
+    return (
+      <CountryPopUp
+        overview={overviewData as any}
+        properties={selectedCountry?.features[0].properties}
       />
-    </main>
-  )
-}
+    );
+  }, [overviewData, selectedCountry]);
 
-export default IndexPage
+  const getSelectedCountryEconomicData = React.useCallback(async () => {
+    if (selectedCountryISO3) {
+      let { data: economic_outlook, error } = await supabase
+        .from("economic_outlook")
+        .select("Value, Indicator (id, title, units)")
 
-export const Head: HeadFC = () => <title>Home Page</title>
+        // Filters
+        .eq("Country", selectedCountryISO3)
+        .eq("Year", 2020)
+        .or("Indicator.eq.BCA,Indicator.eq.GGXWDG,Indicator.eq.NGDPDPC");
+      setOverviewData(economic_outlook);
+      setOverviewDataFetchLoading(false);
+    }
+  }, [selectedCountryISO3]);
+
+  React.useEffect(() => {
+    getSelectedCountryEconomicData();
+  }, [getSelectedCountryEconomicData]);
+
+  return (
+    <MapboxReact
+      sources={
+        dataLoading
+          ? undefined
+          : [
+              {
+                id: "countries",
+                type: "geojson",
+                source: countriesBoundries,
+                layer: countriesPolygonClustersLayer,
+              },
+              {
+                id: "selected_country",
+                type: "geojson",
+                source: selectedCountry,
+                layer: selectedCountryPolygonClustersLayer,
+              },
+            ]
+      }
+      interactiveLayerIds={["countries"]}
+      onClick={(data, position) => {
+        updateSelectedCountry(
+          data as mapboxgl.MapboxGeoJSONFeature[],
+          position
+        );
+      }}
+    >
+      <Popup show={true} lngLat={lngLat ?? undefined}>
+        {popUp()}
+      </Popup>
+    </MapboxReact>
+  );
+};
+
+export default MapPage;
